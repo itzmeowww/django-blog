a81377010084ee464d62c2f8d0107c98d762bd92
itzmeowww <winthanasan@gmail.com>
2020-04-17 07:57:09 +0700

    Add CSS

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# django-test
+# django blog app
+The Django Lessons From [Corey Schafer](https://www.youtube.com/watch?v=UmljXZIypDc&list=PL-osiE80TeTtoQCKZ03TU5fNfx2UY6U4p)
+
+# Get start
+    pip install -r requirements
+    python manage.py migrate
+    python manage.py runserver
--- blog/views.py
+++ blog/views.py
@@ -18,14 +18,14 @@ class PostListView(ListView):
     template_name = 'blog/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
-    paginate_by = 5
+    paginate_by = 7
 
 
 class UserPostListView(ListView):
     model = Post
     template_name = 'blog/user_post.html'
     context_object_name = 'posts'
-    paginate_by = 5
+    paginate_by = 7
 
     def get_queryset(self):
         user = get_object_or_404(User,username = self.kwargs.get('username'))
